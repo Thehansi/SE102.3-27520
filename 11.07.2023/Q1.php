bc327838d2867a33a3ec1a175641ebb80539a483
Thehansi <134836021+Thehansi@users.noreply.github.com>
2023-07-11 19:16:51 +0530

    Add files via upload

---
+++ 11.07.2023/Q1.php
@@ -0,0 +1,12 @@
+<?php
+
+$x = 10;
+$y = 20;
+//$sum = $x + $y;
+
+echo "Sum: ".($x + $y)."<br>";
+echo "Substraction: ".($x-$y)."<br>";
+echo "Multiplication: ".($x*$y)."<br>";
+echo "Divition: ".($x/$y)."<br>";
+
+?>
